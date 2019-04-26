e05fcfd8ec1b99170915d5e0867f5c52ab734150
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-04-26 10:24:34 -0400

    Add the script that compiles and installs the four plugins.

---
+++ plugins-src/plugins-compile-and-install.sh
@@ -0,0 +1,8 @@
+python3 -m compileall .
+mkdir -p ../plugins
+cp __pycache__/*.pyc ../plugins
+mv ../plugins/frequencies1.cpython-36.pyc ../plugins/frequencies1.pyc
+# TODO: move the second frequencies plugin
+mv ../plugins/words1.cpython-36.pyc ../plugins/words1.pyc
+# TODO: move the second words plugin
+mv ../plugins/words2.cpython-36.pyc ../plugins/words2.pyc
