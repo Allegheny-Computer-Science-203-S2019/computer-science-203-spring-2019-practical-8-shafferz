93115a2a81102e0b880555c2431fb938935e1ab9
Zac Shaffer <shafferz@allegheny.edu>
2019-04-29 10:21:51 -0400

    Completed TODOs and reformatted, pass pylint and flake8

--- termfrequency/compute_tf_plugins.py
+++ termfrequency/compute_tf_plugins.py
@@ -4,21 +4,13 @@ import sys
 import configparser
 import importlib.machinery
 
-# TODO: Make sure to complete all of the steps in the following file
-# TODO: Don't forget that the program will not work until you compile and load the plugins
-
 
 def load_plugins():
     """Load the compiled Python plugins specified in the configuration file"""
     config = configparser.ConfigParser()
-    # TODO: Add the name of the configuration file
-    config.read("")
-    # TODO: Add the name of the plugin that will load
-    # either words1.pyc or words2.pyc
-    words_plugin = config.get("Plugins", "")
-    # TODO: Add the name of the plugin that will load
-    # either frequencies1.pyc or frequencies2.pyc
-    frequencies_plugin = config.get("Plugins", "")
+    config.read("config.ini")
+    words_plugin = config.get("Plugins", "words1.pyc")
+    frequencies_plugin = config.get("Plugins", "frequencies1.pyc")
     # pylint: disable=global-variable-undefined
     global tfwords, tffreqs
     # pylint: disable=no-value-for-parameter
@@ -32,7 +24,7 @@ def load_plugins():
 
 
 if __name__ == "__main__":
-    # TODO: Make the call to the function that will load the plugins
+    load_plugins()
     word_freqs = tffreqs.top25(tfwords.extract_words(sys.argv[1]))
     for (w, c) in word_freqs:
         print(w, " - ", c)
